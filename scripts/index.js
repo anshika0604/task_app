7d2dc766e2af03b9e4faa7829140e42cc1a86bf6
Anshika <anshikanegi.cs19@gmail.com>
2023-03-22 13:40:50 +0530

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -3,36 +3,38 @@ const state = {
 };
 
 // DOM Manipulations
-const taskModel = document.querySelector(".task__modal__body");
-//console.log(taskModel);
+const taskModal = document.querySelector(".task__modal__body");
 const taskContent = document.querySelector('.task__content');
 
 
 // to create a card on the home page
-const htmlTaskContent = (id, title, description, type, url) => `
+const htmlTaskContent = ({id, title, description, type, url}) => `
     <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
         <div class='card shadow-sm task__card'>
-            <div class='card-header d-flex justify-content-end task__card__header' >
+            <div class='card-header d-flex gap-2 justify-content-end task__card__header' >
                 <button type='button' class='btn btn-outline-info mr-2' name=${id}>
                 <i class='fa fa-pencil-alt' name='${id}'></i>
                 </button>
-                <button type='button' class='btn btn-outline-danger mr-2' name=${id}>
+                <button type='button' class='btn btn-outline-danger mr-2' name=${id} onclick='deleteTask.apply(this, arguments)'>
                 <i class='fa fa-trash-alt' name='${id}'></i>
                 </button>
-
-                <div class='card-body'>
-                    ${
-                        url && `<img width='100%' src=${url} alt='card image here' class='card-image-top md-3 rounded-lg' />`
-                    }
-                    <h4 class='task__card__title'>${title}</h4>
-                    <p class='description trim-3-lines text-muted' data-gram-editor='false'>${description}</p>
-                    <div class='tags text-white d-flex flex-wrap'>
-                        <span class='badge bg-primary m-1'>${type}</span>
-                    </div>
+            </div>
+            <div class='card-body'>
+                ${
+                    url 
+                    ? `<img width='100%' src=${url} alt='card image here' class='card-image-top md-3 rounded-lg' />`
+                    : `<img width='100%' src="https://www.chanchao.com.tw/VietnamPrintPack/images/default.jpg" alt='card image here' class='card-image-top md-3 rounded-lg' />`
+                }
+                <h4 class='task__card__title'>${title}</h4>
+                <p class='description trim-3-lines text-muted' data-gram_editor='false'>${description}</p>
+                <div class='tags text-white d-flex flex-wrap'>
+                    <span class='badge bg-primary m-1'>${type}</span>
                 </div>
-                <div class='card-footer'>
-                <button type='button' class='btn btn-outline-primary float-right' data-bs-toggle='modal' data-bs-target='showTask'>Show Task</button>
-                </div>
+            </div>
+            <div class='card-footer'>
+                <button type='button' class='btn btn-outline-primary float-right' data-bs-toggle='modal' data-bs-target='#showTask' 
+                id=${id}
+                onclick='openTask.apply(this, arguments)'>Open Task</button>
             </div>
         </div>
     </div>
@@ -41,17 +43,18 @@ const htmlTaskContent = (id, title, description, type, url) => `
 
 // Dynamic Modals(Cards) on our home page
 
-const htmlModalContent = ({id, title, description, type, url}) => {
+const htmlModalContent = ({ id, title, description, url }) => {
     const date = new Date(parseInt(id));
     return `
-        <div id=${id}>
+        <div id = ${id}>
         ${
-            url && `<img width='100%' src=${url} alt='card image here' class='img-fluid place__holder__image mb-3' />`
+            url 
+            ? `<img width='100%' src=${url} alt='card image here' class='img-fluid place__holder__image mb-3' />`
+            : `<img width='100%' src="https://www.chanchao.com.tw/VietnamPrintPack/images/default.jpg" alt='card image here' class='img-fluid place__holder__image mb-3' />`
         }
         <strong class='text-sm text-muted'>Created on ${date.toDateString()}</strong>  
         <h2 class='my-3'>${title}</h2>
         <p class='lead'>${description}</p>
-        <span class='badge bg-primary m-1'>${type}</span>
         </div>
     `;
 };
@@ -59,15 +62,15 @@ const htmlModalContent = ({id, title, description, type, url}) => {
 //Updating LocalStorage
 
 const updateLocalStorage = () => {
-    localStorage.setItem('task',JSON.stringify({
-        tasks:state.taskList,
+    localStorage.setItem("task",JSON.stringify({
+        tasks: state.taskList,
     }));
 };
 
 // to get data or card on your ui from localStorage
 const loadInitialData = () => {
     const localStorageCopy = JSON.parse(localStorage.task);
-    if(localStorageCopy) state.taskList = localStorageCopy.task;
+    if(localStorageCopy) state.taskList = localStorageCopy.tasks;
 
     state.taskList.map((cardData) => {
         taskContent.insertAdjacentHTML("beforeend",htmlTaskContent(cardData));
@@ -82,7 +85,7 @@ const handleSubmit = (event) => {
         type: document.getElementById("taskType").value,
         description: document.getElementById("taskDescription").value,
     };
-    if(input.title == '' || input.type == '' || input.description == '') {
+    if(input.title === "" || input.type === "" || input.description === "") {
         return alert("Please fill all the fields");
     }
     taskContent.insertAdjacentHTML("beforeend",htmlTaskContent({
@@ -93,3 +96,29 @@ const handleSubmit = (event) => {
     //update localStorage
     updateLocalStorage();
 };
+
+// Function for open Task
+const openTask = (e) => {
+    // pop up the current one
+    if (!e) e = window.event;
+
+    //find and open correct card
+    const getTask = state.taskList.find(({ id }) => 
+        id === e.target.id
+    );
+    taskModal.innerHTML = htmlModalContent(getTask);
+};
+
+//  Deleting a task 
+const deleteTask = (e) => {
+    if (!e) e = window.event;
+
+    const targetId = e.target.getAttribute('name');
+    //console.log(targetId);
+    const type = e.target.tagName;
+    //console.log(type);
+
+    const removeTask = state.taskList.filter(({ id }) => id !== targetId);
+    //console.log(removeTask);
+
+}
